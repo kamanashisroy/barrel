a7ee5dac9f56489890c61f78442cbfd53dd1392e
Kamanashis Roy Shuva <kamanashisroy+aroopcode@gmail.com>
2013-07-11 16:17:04 +0600

    form creation and submission is working

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 
-filedb_get_test:
+campaign:
+	echo "get campaign campaign 11\nq" | ./playtool
+
+db_get_test:
 	echo "get test mango 1\nq" | ./playtool
 
-filedb_insert_test:
+db_insert_test:
 	echo "insert test mango ( name => jelly )\nq" | ./playtool
 	
--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 include("includes/config.inc");
 include("includes/string/simple.inc");
 include("includes/forms/build.inc");
+include("includes/forms/submit.inc");
 include("includes/modules/hook.inc");
 include("includes/modules/load.inc");
 include("includes/menu/build.inc");
 include("includes/theme/build.inc");
 
+$project['debug'] = 1;
 ci_theme_build(isset($_REQUEST['q'])?$_REQUEST['q']:"");
 module_all_cleanup();
+
