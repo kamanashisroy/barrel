15b64e6dcf5cd4fe8580fe028b2d28e59125f358
Kamanashis Roy Shuva <kamanashisroy+aroopcode@gmail.com>
2013-07-11 12:37:48 +0600

    We can now insert and get data using filedb

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+filedb_get_test:
+	echo "get test mango 1\nq" | ./playtool
+
+filedb_insert_test:
+	echo "insert test mango ( name => jelly )\nq" | ./playtool
+	
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 include("includes/config.inc");
+include("includes/string/simple.inc");
 include("includes/forms/build.inc");
+include("includes/modules/hook.inc");
 include("includes/modules/load.inc");
 include("includes/menu/build.inc");
 include("includes/theme/build.inc");
 
-ci_theme_build($_REQUEST['q']);
+ci_theme_build(isset($_REQUEST['q'])?$_REQUEST['q']:"");
+module_all_cleanup();
